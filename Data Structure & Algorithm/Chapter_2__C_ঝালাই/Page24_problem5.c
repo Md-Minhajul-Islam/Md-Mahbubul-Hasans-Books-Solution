0c135d09d11d40cdee04a27aec551ba1c18944f6
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-06 17:42:02 +0600

    Page24_problem5.c

---
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/Page24_problem5.c
@@ -0,0 +1,17 @@
+// Q: একটি ত্রিভুজের শীর্ষবিন্দুগুলোর স্থানাঙ্ক দেওয়া আছে, ক্ষেত্রফল প্রিন্ট কর
+
+#include<stdio.h>
+
+int main()
+{
+    double x1, y1, x2, y2, x3, y3;
+    scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3);
+
+    double area = 0.5 * (x1*y2 + x2*y3 + x3*y1 - y1*x2 - y2*x3 - y3*x1); //formula
+    if(area < 0)
+        area = -1.0 * area;
+
+    printf("%lf\n", area);
+
+    return 0;
+}
