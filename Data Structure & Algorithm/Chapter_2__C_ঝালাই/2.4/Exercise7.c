ebb6efbc8edef73518e0ffff7a5a629447ce1ee6
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-11 11:56:46 +0600

    Exercise7.c

--- Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Exercise7.c
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Exercise7.c
@@ -1,8 +1,18 @@
+// Q: 1*n + 2*(n-1)+ ... + n*1;
+
 #include<stdio.h>
  
 int main()
 {
+    int n;
+    scanf("%d", &n);
 
-    
+    int sum = 0;
+    for(int i = 1; i <= n; i++)
+        sum += i*(n-(i-1));
+    printf("%d\n", sum);
+
+    //formula = n*(n*n + 3*n + 2)/6; //For explanation check pdf
     return 0;
 }
+
