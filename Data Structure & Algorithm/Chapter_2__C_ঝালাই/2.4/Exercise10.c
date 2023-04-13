967b53964ab439dda28673c65c0ed51cfdc02569
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-13 23:43:22 +0600

    Exercise10.c

---
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Exercise10.c
@@ -0,0 +1,30 @@
+// Q: তোমাকে n দেওয়া আছে বলতে হবে এটি মৌলিক কি মৌলিক নয়
+#include<stdio.h> 
+int main()
+{
+    long long int n;
+    scanf("%lld", &n);
+
+    int isPrime = 1;
+    if(n == 2 || n == 3)
+        isPrime = 1;
+    else if(n <= 1 || n % 2 == 0 || n % 3 == 0)
+        isPrime = 0;
+    else
+    {
+        for (long long int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+        printf("Prime\n");
+    else
+        printf("Not prime\n");
+
+    return 0;
+}
