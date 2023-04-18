890284a229126c6d4805336fe917513f2df569d5
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-18 22:18:32 +0600

    1083.c

--- Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Timus/1083.c
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Timus/1083.c
@@ -1,7 +1,20 @@
+// Problem link: https://acm.timus.ru/problem.aspx?space=1&num=1083
 #include<stdio.h>
- 
+#include<string.h>
 int main()
 {
-    
+    char marks[20];
+    int n, k;
+    scanf("%d %s", &n, marks);
+    k = strlen(marks);
+    int ans = 1;
+    if(k >= n)
+        ans = n;
+    else
+        for(int i = 0; (n-i*k) > 0; i++)
+            ans *= n-i*k;
+    printf("%d", ans);
     return 0;
 }
+
+
