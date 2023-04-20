9a3f14b1213aaa89d352b158efafe1fcafe5ba2e
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-20 17:48:44 +0600

    1001.c

--- Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/LightOJ/1001.c
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/LightOJ/1001.c
@@ -1,7 +1,17 @@
+// Q: https://lightoj.com/problem/opposite-task
 #include<stdio.h>
- 
 int main()
 {
-    
+   int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int n;
+        scanf("%d", &n);
+        if(n > 10)
+            printf("%d %d\n", 10, n-10);
+        else
+            printf("%d %d\n", 0, n);
+    }
     return 0;
 }
