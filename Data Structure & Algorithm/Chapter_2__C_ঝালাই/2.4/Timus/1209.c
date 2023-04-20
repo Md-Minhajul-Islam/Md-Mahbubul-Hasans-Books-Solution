6f6ccf930134565205378e6c2112e58b7fa55306
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-20 08:30:17 +0600

    1209.c

--- Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Timus/1209.c
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/Timus/1209.c
@@ -1,7 +1,21 @@
-// Q: 
+// Q: https://acm.timus.ru/problem.aspx?space=1&num=1209
+// Solution explained: https://binaryarchives.wordpress.com/2014/06/22/1-10-100-1000-1209-c/ 
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    while(n--)
+    {
+        int k;
+        scanf("%d", &k);
+        if(sqrt(8.0*k - 7) == (int) sqrt(8.0*k-7))
+            printf("%d ", 1);
+        else  
+            printf("%d ", 0);
+    }   
     return 0;
 }
+
+
