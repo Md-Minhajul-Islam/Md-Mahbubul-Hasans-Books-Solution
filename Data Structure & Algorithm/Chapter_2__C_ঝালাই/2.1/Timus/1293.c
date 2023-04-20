d5346a2b7509d03015480bd38440cef8cd59e691
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-20 08:40:08 +0600

    1293.c

---
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.1/Timus/1293.c
@@ -0,0 +1,11 @@
+//problem link: https://acm.timus.ru/problem.aspx?space=1&num=1293
+
+#include<stdio.h>
+int main()
+{
+    int n, a, b;
+    scanf("%d %d %d", &n, &a, &b);
+    int ans = (a * b * 2) * n;
+    printf("%d\n", ans);
+    return 0;
+}
