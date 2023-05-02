4e9c7c0e0d72f60e73ea48f06d926bf73071f557
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-03 00:40:01 +0600

    added 1331.c

---
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.4/LightOJ/1331.c
@@ -0,0 +1,39 @@
+// Q: https://lightoj.com/problem/agent-j
+#include<stdio.h>
+#include<math.h>
+double GetArea(double r, double angle)
+{
+    return 0.5*r*r*angle;   // Partial area formula
+}
+double GetAngle(double a, double b, double c)
+{
+    return acos((b*b + c*c - a*a) / (2.0*b*c));     // Oppposite angle formula
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 1; i <= t; i++)
+    {
+        double r1, r2, r3;
+        scanf("%lf %lf %lf", &r1, &r2, &r3);
+
+        double a, b, c;     // sides of triangle
+        a = r1+r2, b = r2+r3, c = r3+r1;
+        // Calculate the angle created by each straight line that connects each pair of circles' centers
+        double A, B, C;     // angles of triangle
+        A = GetAngle(b, c, a), B = GetAngle(c, a, b), C = GetAngle(a, b, c);
+        // Calculate the area of the triangle
+        double s = (a+b+c) / 2.0;
+        double area = sqrt(s * (s - a) * (s - b) * (s - c));
+        // Deduct area taken by the three circles
+        area -= GetArea(r1, A);
+        area -= GetArea(r2, B);
+        area -= GetArea(r3, C);
+        
+        printf("Case %d: %lf\n", i, area);
+    }
+    return 0;
+}
