0f9b12261da9401a2803fddb898727fbc9563214
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-20 17:57:26 +0600

    edited 1020.c

--- Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.5/Timus/1020.c
+++ Data Structure & Algorithm/Chapter_2__C_ঝালাই/2.5/Timus/1020.c
@@ -1,7 +1,6 @@
 // Q: https://acm.timus.ru/problem.aspx?space=1&num=1020
 // hint: দড়ির দৈর্ঘ্য হবে বহুভুজের পরিসীমা ও একটি পেরেকের পরিসীমার সমান।
 // hint: The length of the rope will be equal to the perimeter of the polygon plus the perimeter of a nail.
-// choose Language: G++ 9.2 x64
 #include<stdio.h>
 #include<math.h>
 int main()
