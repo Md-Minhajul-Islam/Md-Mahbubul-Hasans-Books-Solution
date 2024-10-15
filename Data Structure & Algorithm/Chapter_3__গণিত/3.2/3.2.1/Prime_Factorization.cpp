335bc865d8e3bd84aa440646fb970b30c9b0d12b
Minhaj <minhajulislammuhammad@gmail.com>
2024-10-15 10:58:39 +0600

    added

---
+++ Data Structure & Algorithm/Chapter_3__গণিত/3.2/3.2.1/Prime_Factorization.cpp
@@ -0,0 +1,17 @@
+
+//https://forthright48.com/prime-factorization-of-factorial/
+
+
+void factFactorize ( int n ) {
+    for ( int i = 0; i < prime.size() && prime[i] <= n; i++ ) {
+        int x = n;
+        int freq = 0;
+ 
+        while ( x / prime[i] ) {
+            freq += x / prime[i];
+            x = x / prime[i];
+        }
+ 
+        printf ( "%d^%d\n", prime[i], freq );
+    }
+}
